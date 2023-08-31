659a9610b88a299d431e9d5dcd1d053223802042
Paul LeTourneau <pletourneau62@gmail.com>
2023-08-31 08:56:39 -0700

    test

--- webpack.config.js
+++ webpack.config.js
@@ -10,9 +10,8 @@ module.exports = {
     path: path.resolve(__dirname, 'dist')
   },
   devServer: {
-    static: {
-      directory: path.join(__dirname, "dist"),
-    }
+    contentBase: "./dist",
+  },
   },
   devtool: 'eval-source-map',
   plugins: [
